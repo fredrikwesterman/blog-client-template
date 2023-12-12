da2de6b53c562cfcbd10ddd91022c68a78c6cff1
Fredrik Westerman <fredrikwesterman9@gmail.com>
2023-12-12 11:19:10 +0100

    alot of styling done. and fixes to the update-tags

--- js/post.js
+++ js/post.js
@@ -10,13 +10,15 @@ async function getBlogpost(){
         let resp = await fetch('https://blog-api-assignment.up.railway.app/posts/' + postId)
         let post = await resp.json()
         console.log(post)
+        let postDate = new Date(post.date)
 
-        document.getElementById('post').innerHTML = `
-            <a href="index.html" id="back-to-posts">Back to posts</a> 
-            <h2>${post.title}</h2>
-            <p> Author: ${post.author} | <i>${post.date}</i></p>
-            <p>${post.content}</p>
-            <p>Tags: ${post.tags}</p>
+        document.getElementById('post').innerHTML = ` 
+            <li id="post-card-single" class="post-card">
+                <h2>${post.title}</h2>
+                <p>Author: ${post.author} | <i>${postDate.getFullYear()}-${postDate.getMonth()+1}-${postDate.getDate()}, ${postDate.toLocaleTimeString()}</i></p>
+                <p>${post.content}</p>
+                <p>Tags: ${post.tags}</p>
+            </li>
         `
         
     } catch(error) {
